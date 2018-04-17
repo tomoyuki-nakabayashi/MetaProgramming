a64c67ab50a9227439dcbcab1556026a54a03f77
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-04-17 20:46:00 +0900

    snippets: parameterized inheritance.

--- chapter3/parameterized_inheritance.cpp
+++ chapter3/parameterized_inheritance.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
-#include <cstdlib>
+// Copyright <2018> <Tomoyuki Nakabayashi>
+// This software is released under the Apache 2.0 License, see LICENSE.
 
+#include <cstdint>
+#include <string>
+#include <sstream>
+#include <gtest/gtest.h>
+
+namespace basic_inheritance {
 class Plain {
  public:
     std::string convert(const std::string& str) const {
@@ -24,18 +30,86 @@ class Italic : public Base {
     }
 };
 
-int main() {
+class BasicInheritance : public ::testing::Test {
+};
+
+TEST_F(BasicInheritance, Test) {
   Bold<Plain> b;
   std::string s1 = b.convert("Hello");
-  std::cout << s1 << std::endl;
-  
+  EXPECT_EQ("<b>Hello</b>", s1);
+
   Italic<Plain> i;
   std::string s2 = i.convert("Hello");
-  std::cout << s2 << std::endl;
+  EXPECT_EQ("<i>Hello</i>", s2);
   
   Italic<Bold<Plain>> ib;
   std::string s3 = ib.convert("Hello");
-  std::cout << s3 << std::endl;
-
-  return 0;
+  EXPECT_EQ("<i><b>Hello</b></i>", s3);
 }
+
+}  // namespace basic_inheritance
+
+namespace virtualized_inheritance {
+class Plain {
+ public:
+    virtual std::string convert(const std::string& str) const {
+      return str;
+    }
+};
+
+template <typename Base = Plain>
+class Bold : public Base {
+ public:
+    using Base::Base;  // Inherit constructor.
+
+    std::string convert(const std::string &str) const override {
+      return "<b>" + Base::convert(str) + "</b>";
+    }
+};
+
+template <typename Base = Plain>
+class Italic : public Base {
+ public:
+    using Base::Base;  // Inherit constructor.
+
+    virtual std::string convert(const std::string &str) const override {
+      return "<i>" + Base::convert(str) + "</i>";
+    }
+};
+
+template <typename Base = Plain>
+class Size : public Base {
+ public:
+    using Base::Base;  // Inherit constructor.
+    Size(int32_t s) {
+      std::ostringstream ostrm;
+      ostrm << s;
+      size_ = ostrm.str();
+    }
+
+    virtual std::string convert(const std::string &str) const override {
+      return "<font size='" + size_ + "'>" + Base::convert(str) + "</font>";
+    }
+
+ private:
+    std::string size_;
+};
+
+class VirtualizedInheritance : public ::testing::Test {
+};
+
+TEST_F(VirtualizedInheritance, Test) {
+  Plain p;
+  Bold<> b;
+  Italic<> i;
+  Size<Bold<Italic<>>> sbi(5);
+  Italic<Size<>> is(3);
+
+  EXPECT_EQ("Hello", p.convert("Hello"));
+  EXPECT_EQ("<b>Hello</b>", b.convert("Hello"));
+  EXPECT_EQ("<i>Hello</i>", i.convert("Hello"));
+  EXPECT_EQ("<font size='5'><b><i>Hello</i></b></font>", sbi.convert("Hello"));
+  EXPECT_EQ("<i><font size='3'>Hello</font></i>", is.convert("Hello"));
+}
+
+}  // namespace virtualized_inheritance
