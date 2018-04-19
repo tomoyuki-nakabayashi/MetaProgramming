f162ffa2557a789ba998c62ae975857d4dba7b9f
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-04-19 20:35:18 +0900

    snippets: Add Overload by type.

--- chapter5/sfinae.cpp
+++ chapter5/sfinae.cpp
@@ -5,6 +5,9 @@
 #include <type_traits>
 #include <utility>
 #include <vector>
+#include <list>
+#include <algorithm>
+#include <iterator>
 #include <gtest/gtest.h>
 
 namespace sfinae {
@@ -50,6 +53,33 @@ struct B {};
 TEST_F(SFINAE, IsAssignable) {
   static_assert(!is_assignable<A>::value, "Cannot substitute.");
   static_assert( is_assignable<B>::value, "Can substitute.");
+}
+
+struct has_sort_member_impl {
+  template <class T>
+  static auto check(T*) -> decltype(
+    std::declval<T>().sort(),
+    std::true_type()
+  );
+
+  template <class T>
+  static auto check(...) -> std::false_type;
+};
+
+template <class T>
+struct has_sort_member :
+  decltype(has_sort_member_impl::check<T>(nullptr)) {};
+
+template <class Sortable,
+  typename std::enable_if<has_sort_member<Sortable>::value>::type* = nullptr>
+void sort(Sortable &x) {
+  x.sort();
+}
+
+template <class Range,
+  typename std::enable_if<!has_sort_member<Range>::value>::type* = nullptr>
+void sort(Range& r) {
+  std::sort(std::begin(r), std::end(r));
 }
 
 }  // namespace sfinae
