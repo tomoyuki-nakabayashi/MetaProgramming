b01b03ee590770e0336d952906cddfa06587b1b9
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-05-18 13:17:52 +0900

    snippets: C++ Template Technique pp.192-195 Concept based design.

--- chapter7/concept.cpp
+++ chapter7/concept.cpp
@@ -40,4 +40,43 @@ TEST_F(CONCEPT, Min) {
   // X x = min::min(X(), X());  // compile error
 }
 
+class point {
+    double x_ = 0;
+    double y_ = 0;
+ public:
+    point() = default;
+    point(double x, double y) : x_(x), y_(y) {}
+
+    double x() const { return x_; }
+    double y() const { return y_; }
+};
+
+point operator-(const point& a, const point& b) {
+  return point(a.x() - b.x(), a.y() - b.y());
+}
+
+template <class T>
+struct point_traits {
+  static double x(const T& p) { return p.x(); }
+  static double y(const T& p) { return p.y(); }
+
+  static T subtract(const T& a, const T& b) {
+    return T(a.x() - b.x(), a.y() - b.y());
+  }
+};
+
+template <class Point>
+double distance(Point a, Point b) {
+  typedef point_traits<Point> traits;
+  const Point d = traits::subtract(a, b);
+  return std::sqrt(traits::x(d) * traits::x(d) + traits::y(d) * traits::y(d));
+}
+
+TEST_F(CONCEPT, Point) {
+  point a(0.0, 0.0);
+  point b(3.0, 3.0);
+
+  EXPECT_NEAR(4.24264, distance(a, b), 0.00001);
+}
+
 }  // namespace concept
